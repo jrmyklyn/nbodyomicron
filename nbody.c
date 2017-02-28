dc8bc4fbf93c71dff8fb36e2b71989cf60771f4d
nthnhkmn <nthnhkmn@dordt.edu>
2017-02-28 17:53:51 -0600

    Animation Edits

--- nbody.c
+++ nbody.c
@@ -7,9 +7,11 @@
 int n = 2;
 double dt = 60 * 60; // seconds
 int steps = 24 * 27;
-double angle = 0.0;
+double angle = 0;
+double aspectRatio = 0;
 
 #define GRAVITY_CONST 6.67408E-20
+#define VIEW_ANGLE M_PI/6
 
 typedef struct {
 	double mass, // kg
@@ -58,61 +60,58 @@ void moveBodies() {
 	}
 }
 
-void displayDraw() {
+void displayDrawCallback() {
+	// Set camera angle, height, width, depth
+	double maxDistance = 200;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if(aspectRatio < 1)
+		glFrustum(-maxDistance, maxDistance, -maxDistance / aspectRatio, maxDistance / aspectRatio, 2 * maxDistance, 4 * maxDistance);
+	else
+		glFrustum(-maxDistance * aspectRatio, maxDistance * aspectRatio, -maxDistance, maxDistance, 2 * maxDistance, 4 * maxDistance);
+	gluLookAt(3 * maxDistance * cos(VIEW_ANGLE), 0, 3 * maxDistance * sin(VIEW_ANGLE), 0, 0, 0, 0, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
 		glRotated(angle, 0, 0, 1);
-		glColor3d(0.0, 1.0, 0.0);
+		glColor3d(0, 1, 0);
 		glPushMatrix();
-			glTranslated(0.0, 0.0, 0.0);
-			glutSolidSphere(25.0, 50, 50);
+			glTranslated(0, 0, 0);
+			glutSolidSphere(30, 20, 20);
 		glPopMatrix();
-		glColor3d(1.0, 0.0, 0.0);
+		glColor3d(1, 0, 0);
 		glPushMatrix();
-			glTranslated(-50.0, -50.0, -50.0);
-			glutSolidSphere(10.0, 50, 50);
+			glTranslated(-50, -50, -50);
+			glutSolidSphere(20, 20, 20);
 		glPopMatrix();
 		glPushMatrix();
 			glBegin(GL_LINES);
-			glColor3f(1.0, 0.0, 0.0);
-			glVertex3f(-1000.0, 0.0, 0.0);
-			glVertex3f(1000.0, 0.0, 0.0);
-			glColor3f(0.0, 1.0, 0.0);
-			glVertex3f(0.0, -1000.0, 0.0);
-			glVertex3f(0.0, 1000.0, 0.0);
-			glColor3f(0.0, 0.0, 1.0);
-			glVertex3f(0.0, 0.0, -1000.0);
-			glVertex3f(0.0, 0.0, 1000.0);
+			glColor3f(1, 0, 0);
+			glVertex3f(-maxDistance, 0, 0);
+			glVertex3f(maxDistance, 0, 0);
+			glColor3f(0, 1, 0);
+			glVertex3f(0, -maxDistance, 0);
+			glVertex3f(0, maxDistance, 0);
+			glColor3f(0, 0, 1);
+			glVertex3f(0, 0, -maxDistance);
+			glVertex3f(0, 0, maxDistance);
 			glEnd();
 		glPopMatrix();
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
-void displayIdle() {
+void displayIdleCallback() {
 	angle = fmod(angle + 1.0, 360.0);
 	glutPostRedisplay();
 }
 
-void displayReshape(int width, int height) {
+void displayReshapeCallback(int width, int height) {
 	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45, (double)width / (double)height, 500, 1000);
-	gluLookAt(600, 0, 450, 0, 0, 0, 0, 0, 1);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	aspectRatio = (double)width / (double)height;
 }
-
-const GLfloat light_ambient[] = {0.0f, 0.0f, 0.0f, 1.0f};
-const GLfloat light_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat light_position[] = {2.0f, 5.0f, 5.0f, 0.0f};
-
-const GLfloat mat_ambient[] = {0.7f, 0.7f, 0.7f, 1.0f};
-const GLfloat mat_diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
-const GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-const GLfloat high_shininess[] = {100.0f};
 
 int main(int argc, char *argv[]) {
 	// Set up parameters
@@ -135,7 +134,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
-	// Initialize bodies
+	// Load Bodies
 	bodies = (body_t *)malloc(n * sizeof(body_t));
 	bodies[0].x = -2.675244393757571E4;
 	bodies[0].y = -3.991567778823322E5;
@@ -154,7 +153,7 @@ int main(int argc, char *argv[]) {
 	bodies[1].mass = 5.972E24;
 	bodies[1].radius = 6371;
 	
-	// Run program
+	// Start Model
 	while(steps > 0) {
 		printBody(bodies[0], "Moon");
 		printBody(bodies[1], "Earth");
@@ -165,37 +164,28 @@ int main(int argc, char *argv[]) {
 		steps--;
 	}
 	
-	// Display
+	// Setup Display Window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClearColor(1, 1, 1, 1);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(50, 50);
 	glutCreateWindow("N-Body Simulation");
 	
-	glutDisplayFunc(displayDraw);
-	glutReshapeFunc(displayReshape);
-	glutIdleFunc(displayIdle);
+	// Initialize Display Callbacks
+	glutDisplayFunc(displayDrawCallback);
+	glutReshapeFunc(displayReshapeCallback);
+	glutIdleFunc(displayIdleCallback);
 	
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-	
+	// Setup Lighting
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_LIGHTING);
 	
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
-	
-	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+	// Setup Other Display Options
+	glEnable(GL_CULL_FACE); // Enabled for efficiency
+	glEnable(GL_DEPTH_TEST); // Enabled for the depth buffer
 	
 	glutMainLoop();
 }
